b5b7550f28edffc45be2fc9bfbaac0ce99ae6a70
Bart Wijgers <b.wijgers@students.uu.nl>
2021-11-22 18:43:24 +0100

    Add html output to criterion

--- bench/Main.hs
+++ bench/Main.hs
@@ -5,12 +5,19 @@ module Main where
 import DotProduct (dotpBenches)
 
 import Criterion.Main
+import Criterion.Types
+
+config :: Config
+config = defaultConfig {
+    timeLimit = 2.0
+  , reportFile = Just $ "accelerate-benchmarks.html"
+  }
 
 main :: IO ()
 main = 
   do
     dotp <- dotpBenches
-    defaultMain [
+    defaultMainWith config [
         dotp
       ]
 
